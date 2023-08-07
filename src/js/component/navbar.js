7cab69115b4da0f3e21f2d43b7a24e049910f5ae
Miguel-web <miguelgilf@gmail.com>
2023-08-07 01:49:59 +0000

    ready up

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
     <nav className="navbar navbar-light bg-light m-0 d-flex justify-content-end">
       <div className="ml-auto">
         <Link to="/GetCard">
-          <button className="btn btn-success me-2">Add new contact</button>
+          <button className="btn btn-success me-2">Add Contact</button>
         </Link>
       </div>
     </nav>
